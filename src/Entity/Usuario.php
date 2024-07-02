2c93b1597975853de93f23f2cdf036b8bc2972ac
Marco Pescarolo <pescarolom@gmail.com>
2024-07-01 20:40:29 -0400

    Cree la entidad usuario

---
+++ src/Entity/Usuario.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsuarioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UsuarioRepository::class)]
+class Usuario
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nombre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Mail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Contrasena = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->Nombre;
+    }
+
+    public function setNombre(string $Nombre): static
+    {
+        $this->Nombre = $Nombre;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->Mail;
+    }
+
+    public function setMail(string $Mail): static
+    {
+        $this->Mail = $Mail;
+
+        return $this;
+    }
+
+    public function getContrasena(): ?string
+    {
+        return $this->Contrasena;
+    }
+
+    public function setContrasena(string $Contrasena): static
+    {
+        $this->Contrasena = $Contrasena;
+
+        return $this;
+    }
+}
