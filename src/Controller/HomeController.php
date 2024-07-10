d4dd8fccebe90b5a137c6418bbe03fcf4ca8d4c4
lucas-rafaul <rafaullucas@gmail.com>
2024-07-10 20:14:05 -0300

    se arreglo el tema de las imagenes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,19 +3,31 @@
 namespace App\Controller;
 
 use App\Repository\PosteoRepository;
+use App\Entity\Categoria;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(PosteoRepository $posteoRepository): Response
+    public function index(Request $request, PosteoRepository $posteoRepository, EntityManagerInterface $entityManager): Response
     {
-        $posteos = $posteoRepository->findBy([], ['fecha' => 'DESC'], 10);
+        $categoriaId = $request->query->get('categoria');
+        $posteos =         // Fetch posteos ordered by newest first, either filtered by categoria or all
+            $posteos = $posteoRepository->findBy(
+                $categoriaId ? ['categoria' => $categoriaId] : [],
+                ['fecha' => 'DESC']
+            );
+
+        $categorias = $entityManager->getRepository(Categoria::class)->findAll();
 
         return $this->render('home.html.twig', [
             'posteos' => $posteos,
+            'categorias' => $categorias,
+            'selectedCategoria' => $categoriaId,
         ]);
     }
 }
